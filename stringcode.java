6132561e5dd842c81f33b8aba28573d430715cf2
CHETAN SHELAKE <141062766+IamChetanShelake@users.noreply.github.com>
2024-07-09 09:08:33 +0530

    Add files via upload

---
+++ stringcode.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class stringcode{
+public static void main(String aa[]){
+	Scanner sc = new Scanner(System.in);
+	String str = sc.nextLine();
+	
+	char c[] = str.toCharArray();
+		
+	
+		for(char a : c){
+			System.out.print(a+" ");
+			}
+	
+	
+	
+}
+}
